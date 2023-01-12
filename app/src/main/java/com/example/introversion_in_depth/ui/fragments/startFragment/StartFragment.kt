d6a0e93c73153f17bdd57365cf3cd3e8ae54a132
Bruno Santos <57073513+Bruno213@users.noreply.github.com>
2023-01-12 19:48:25 -0300

    Creating Faq screen.

--- app/src/main/java/com/example/introversion_in_depth/ui/fragments/startFragment/StartFragment.kt
+++ app/src/main/java/com/example/introversion_in_depth/ui/fragments/startFragment/StartFragment.kt
@@ -93,7 +93,6 @@ class StartFragment: BaseFragment<FragmentStartBinding>(), View.OnClickListener 
             }
 
             override fun onNothingSelected(parent: AdapterView<*>?) {
-
             }
         }
 
